37ca4ceb02bb8e3edb6a90112db98724766f8fbe
MarioV312 <00118018@uca.edu.sv>
2019-11-01 11:28:46 -0600

    Updates

---
+++ precache-manifest.e0c5bea469006feff69adae1c2dd1037.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e047b5aa649eaf6a13480f831dbf461",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "da741469d9a14e458a6a",
+    "url": "/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "d783c66b66b346a53af7",
+    "url": "/tictactoe/static/js/2.475f74e8.chunk.js"
+  },
+  {
+    "revision": "da741469d9a14e458a6a",
+    "url": "/tictactoe/static/js/main.df43ed25.chunk.js"
+  },
+  {
+    "revision": "a33ad88705da872849b2",
+    "url": "/tictactoe/static/js/runtime-main.6f2fa403.js"
+  }
+]);
